dce53bd3860c1f37d97caf7132507497a6ad6735
kristyanYochev <kris.yochev@gmail.com>
2019-01-22 19:43:25 +0200

    Added carousel

---
+++ scripts/carousel.js
@@ -0,0 +1,57 @@
+class Carousel {
+    constructor(parentElement) {
+        this.parentElement = parentElement
+        this.slides = parentElement.querySelectorAll(".carousel__slide")
+        this.activeSlide = 0
+
+        this.interval = null
+        this.rotating = false
+
+        this.setSlide = this.setSlide.bind(this)
+        this.nextSlide = this.nextSlide.bind(this)
+        this.start = this.start.bind(this)
+    }
+
+    setSlide(slideNumber) {
+        if (slideNumber >= this.slides.length || slideNumber < 0) {
+            console.error(`Invalid slide number: ${slideNumber}`)
+            return;
+        }
+
+        this.activeSlide = slideNumber;
+
+        // Deactivate all slides
+        this.slides.forEach(slide => {
+            slide.classList.remove("active")
+        })
+
+        this.slides[slideNumber].classList.add("active")
+    }
+
+    nextSlide() {
+        if (this.activeSlide + 1 >= this.slides.length) {
+            this.setSlide(0)
+        } else {
+            this.setSlide(this.activeSlide + 1)
+        }
+    }
+
+    start(delay) {
+        if (this.rotating) {
+            console.error("Unable to start carousel: already started!")
+            return;
+        }
+        this.rotating = true
+        this.interval = setInterval(this.nextSlide, delay)
+    }
+
+    stop() {
+        this.rotating = false
+        clearInterval(this.interval)
+    }
+}
+
+(() => {
+    var carousel = new Carousel(document.getElementById("hacktues3-carousel"))
+    carousel.start(2000)
+})()
